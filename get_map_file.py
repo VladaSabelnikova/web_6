895316de00f5a5785cb240ec7a1ac3432445a1e9
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-02-02 22:46:35 +0300

    Вторая задача решена + изменена архитектура первой
    Теперь map_file делается в отдельной функции
    get_map_file

---
+++ get_map_file.py
@@ -0,0 +1,14 @@
+import pathlib
+
+from get_response import get_response_from_static_api
+from show_img import show_img
+
+
+def get_map_file(
+    longitude: str = '141.318090',
+    latitude: str = '-28.514865',
+    delta: str = '50.992457'
+) -> pathlib.Path:
+    response_static = get_response_from_static_api(longitude, latitude, delta)
+    map_file = show_img(response_static)
+    return map_file
--- task_2/task_2.py
+++ task_2/task_2.py
@@ -1,6 +1,7 @@
 import pygame
 
 from get_map_file import get_map_file
+from settings import KEY_UP, KEY_DOWN
 
 
 def main() -> None:
@@ -10,12 +11,28 @@ def main() -> None:
     pygame.init()
     pygame.display.set_caption('Большая задача')
     screen = pygame.display.set_mode((600, 450))
+
     running = True
+    step = 0.5
+    old_delta = delta
+
     while running:
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 running = False
+
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == KEY_UP:
+                    delta = str(min(float(delta) * step, 50))
+
+                elif event.key == KEY_DOWN:
+                    delta = str(max(float(delta) / step, 0.0001))
+
+        if old_delta != delta:
+            old_delta = delta
+            map_file = get_map_file(longitude, latitude, delta)
 
         screen.blit(pygame.image.load(map_file), (0, 0))
         pygame.display.flip()
