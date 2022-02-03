c7c2fd54734a259a53f0be5bb2cb9f600dcbe5a0
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-02-03 18:16:11 +0300

    Убрала лишние импорты в drop_down_menu.py
    Всё

--- pygame_gui_elements/drop_down_menu.py
+++ pygame_gui_elements/drop_down_menu.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 import pygame
 import pygame_gui
 
@@ -7,7 +5,6 @@ import pygame_gui
 def create_drop_down_menu(
     manager: pygame_gui.UIManager
 ) -> pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu:
-
     drop_down_menu = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(
         options_list=['map', 'sat', 'sat,skl', 'sat,trf,skl', 'map,trf,skl'],
         starting_option='map',
